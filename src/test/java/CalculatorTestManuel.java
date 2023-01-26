93d78c899ae433ad1ba4ebc44a0f62bcf6bbe38b
Jeff <Jeffagri-commerce.fr>
2023-01-26 14:16:46 +0100

    first commit

---
+++ src/test/java/CalculatorTestManuel.java
@@ -0,0 +1,28 @@
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestManuel {
+    Calculator calculator = new Calculator();
+    @Test
+    @DisplayName("1+2=3")
+    void testAdd() {
+
+        int retour = calculator.add(1, 2);
+        assertEquals(3, retour,"1 +2 = 3");
+        // Assert
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,-1,0"
+    })
+    void testAll(int a , int b,int expectedResult) {
+        assertEquals(3, calculator.add(1, 2));
+    }
+}
